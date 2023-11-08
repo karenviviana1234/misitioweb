f15ffa5ab4b376ac0ca54bc83f5d3f810c3d9020
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-11-08 16:44:19 -0500

    amazon

--- js/amazon.js
+++ js/amazon.js
@@ -5,30 +5,36 @@ let imgtub4 = document.getElementById("imgtub4");
 let imgtub5 = document.getElementById("imgtub5");
 let imgtub6 = document.getElementById("imgtub6");
 let imgtub7 = document.getElementById("imgtub7");
-let imgPrincipal = document.getElementById("imgPrincipal");
+let imgMain = document.getElementById("imgPrincipal");
+let boxmodal = document.getElementById("box-modal");
+let modal = document.getElementById("modal");
+let mainContent = document.getElementById("mainContent");
+let btnclose = document.getElementById("btnclose");
 
-imgtub1.addEventListener("click", ()=>  {
-  imgPrincipal.classList.add("main-img-principal-opacity");
-  imgPrincipal.setAttribute("src", "img/amazon1.jpg");
+window.onload = () => {
+  boxmodal.classList.remove("visible")
+}
+imgtub1.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/amazon1.jpg");
 });
-imgtub2.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon2.jpg");
+imgtub2.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/amazon2.jpg");
 });
-imgtub3.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon3.jpg");
+imgtub3.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/amazon3.jpg");
 });
-imgtub4.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon4.jpg");
+imgtub4.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/amazon4.jpg");
 });
-imgtub4.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon4.jpg");
+imgtub5.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/amazon5.jpg");
 });
-imgtub5.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon5.jpg");
+imgtub6.addEventListener("click",() => {
+  imgMain.setAttribute("src", "img/amazon6.jpg");
 });
-imgtub6.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon6.jpg");
+btnclose.addEventListener("click", function() {
+  mainContent.style.display = "none";
 });
-imgtub7.addEventListener("click", ()=>  {
-  imgPrincipal.setAttribute("src", "img/amazon7.jpg");
-});
+function abrir(){
+  boxmodal.classList.add("visible")
+}
