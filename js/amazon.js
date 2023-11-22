4000cf1877410a1b2b2a89636449bd1c789ec421
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-11-22 10:46:36 -0500

    fondo amazon

--- js/amazon.js
+++ js/amazon.js
@@ -1,3 +1,117 @@
+let start1 = document.getElementById("start1")
+let start2 = document.getElementById("start2")
+let start3 = document.getElementById("start3")
+let start4 = document.getElementById("start4")
+let start5 = document.getElementById("start5")
+let puntuacion = document.getElementById("puntuacion").innerText = 3
+    
+    if(puntuacion >= 0 && puntuacion <= 0.2) {
+        start1.setAttribute("class", "fa-regular fa-star")
+        start2.setAttribute("class", "fa-regular fa-star")
+        start3.setAttribute("class", "fa-regular fa-star")
+        start4.setAttribute("class", "fa-regular fa-star")
+        start5.setAttribute("class", "fa-regular fa-star")
+    }
+    else if(puntuacion >= 0.3 && puntuacion <= 0.7) {
+        start1.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
+        start2.setAttribute("class", "estrella fa-regular fa-star")
+        start3.setAttribute("class", "estrella fa-regular fa-star")
+        start4.setAttribute("class", "fa-regular fa-star")
+        start5.setAttribute("class", "fa-regular fa-star")
+    }
+    else if(puntuacion >= 0.8 && puntuacion <= 1.2) {
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-regular fa-star")
+        start3.setAttribute("class", "estrella fa-regular fa-star")
+        start4.setAttribute("class", "estrella fa-regular fa-star")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 1.3 && puntuacion <= 1.7){
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
+        start3.setAttribute("class", "estrella fa-regular fa-star")
+        start4.setAttribute("class", "estrella fa-regular fa-star")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 1.8 && puntuacion <= 2.2) {
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-regular fa-star")
+        start4.setAttribute("class", "estrella fa-regular fa-star")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 2.3 && puntuacion <= 2.7) {
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
+        start4.setAttribute("class", "estrella fa-regular fa-star")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 2.8 && puntuacion <= 3.2){
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-solid fa-star")
+        start4.setAttribute("class", "estrella fa-regular fa-star")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 3.3 && puntuacion <= 3.7) {
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-solid fa-star")
+        start4.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 3.8 && puntuacion <= 4.2) {
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-solid fa-star")
+        start4.setAttribute("class", "estrella fa-solid fa-star")
+        start5.setAttribute("class", "estrella fa-regular fa-star")
+    }
+    else if(puntuacion >= 4.3 && puntuacion <= 4.7) {
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-solid fa-star")
+        start4.setAttribute("class", "estrella fa-solid fa-star")
+        start5.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
+    }
+    else if(puntuacion >= 4.8 && puntuacion <= 5){
+        start1.setAttribute("class", "estrella fa-solid fa-star")
+        start2.setAttribute("class", "estrella fa-solid fa-star")
+        start3.setAttribute("class", "estrella fa-solid fa-star")
+        start4.setAttribute("class", "estrella fa-solid fa-star")
+        start5.setAttribute("class", "estrella fa-solid fa-star")
+    }
+
+
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
+let img5 = document.getElementById("img5");
+let img6 = document.getElementById("img6");
+let img7 = document.getElementById("img7");
+let imgprin = document.getElementById("imgprin");
+img1.addEventListener("mouseover",() => {
+  imgprin.setAttribute("src", "img/amazon1.jpg");
+});
+img2.addEventListener("mouseover",() => {
+  imgprin.setAttribute("src", "img/amazon2.jpg");
+});
+img3.addEventListener("mouseover",() => {
+  imgprin.setAttribute("src", "img/amazon3.jpg");
+});
+img4.addEventListener("mouseover",() => {
+  imgprin.setAttribute("src", "img/amazon4.jpg");
+});
+img5.addEventListener("mouseover",() => {
+  imgprin.setAttribute("src", "img/amazon5.jpg");
+});
+img6.addEventListener("mouseover",() => {
+  imgprin.setAttribute("src", "img/amazon6.jpg");
+});
+
+
 let imgtub1 = document.getElementById("imgtub1");
 let imgtub2 = document.getElementById("imgtub2");
 let imgtub3 = document.getElementById("imgtub3");
@@ -39,29 +153,3 @@ window.onload = () => {
   boxmodal.classList.remove("visible")
 }
 
-let img1 = document.getElementById("img1");
-let img2 = document.getElementById("img2");
-let img3 = document.getElementById("img3");
-let img4 = document.getElementById("img4");
-let img5 = document.getElementById("img5");
-let img6 = document.getElementById("img6");
-let img7 = document.getElementById("img7");
-let imgprin = document.getElementById("imgprin");
-img1.addEventListener("click",() => {
-  imgprin.setAttribute("src", "img/amazon1.jpg");
-});
-img2.addEventListener("click",() => {
-  imgprin.setAttribute("src", "img/amazon2.jpg");
-});
-img3.addEventListener("click",() => {
-  imgprin.setAttribute("src", "img/amazon3.jpg");
-});
-img4.addEventListener("click",() => {
-  imgprin.setAttribute("src", "img/amazon4.jpg");
-});
-img5.addEventListener("click",() => {
-  imgprin.setAttribute("src", "img/amazon5.jpg");
-});
-img6.addEventListener("click",() => {
-  imgprin.setAttribute("src", "img/amazon6.jpg");
-});
